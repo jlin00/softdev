01a3661600de19172214129b00bfcef607ca20e1
Jackie Lin <jlin00@stuy.edu>
2019-09-10 15:08:54 -0400

    wrote another version of the problem activity with help from Peihua Huang period 2, also added comments

--- randomName.py
+++ randomName.py
@@ -1,5 +1,6 @@
 import random
 
+#shortened list of names to test out function
 KREWES = {
     'orpheus': ['Emily', 'Kevin', 'Vishwaa'],
     'rex': ['William', 'Joseph', 'Calvin'],
@@ -8,7 +9,20 @@ KREWES = {
 
 def randomName(team):
     print (KREWES[team][random.randint(0, len(KREWES[team]) - 1)])
+    #references the team specified by the parameter, then obtains a random number based on the team's length and returns the name at that index
 
-randomName('orpheus')
-randomName('rex')
-randomName('endymion')
+#tests
+#randomName('orpheus')
+#randomName('rex')
+#randomName('endymion')
+
+#different from first function because it does not need team as a parameter, automatically chooses a random team and random name
+#this method was explained to me by Peihua Huang from period 2
+def randomName(dict):
+    teams = list(dict.keys()) #turns dictionary into list
+    period = random.choice(teams) #chooses a random index from that list
+    print(random.choice(dict[period])) #prints a random index from chosen team
+
+#tests
+for i in range(0, 10):
+    randomName(KREWES)
