b913d65f22f6f4c52e8963e59b94ccd1b5d76dab
Jackie Lin <jlin00@stuy.edu>
2019-09-11 20:59:43 -0400

    merged code with caoT

--- 02_randomizer/couchPotatoes_linJ-caoT.py
+++ 02_randomizer/couchPotatoes_linJ-caoT.py
@@ -1,28 +1,21 @@
 import random
 
-#shortened list of names to test out function
 KREWES = {
-    'orpheus': ['Emily', 'Kevin', 'Vishwaa'],
-    'rex': ['William', 'Joseph', 'Calvin'],
-    'endymion': ['Grace', "Nahi", 'Derek']
+'orpheus': ['Emily', 'Kevin', 'Vishwaa', 'Eric', 'ray', 'Jesse', 'Tiffany', 'Amanda', 'Junhee', 'Jackie', 'Tyler', 'Emory', 'Ivan', 'Elizabeth', 'Pratham', 'Shaw', 'Eric', 'Yaru', 'Kelvin', 'Hong Wei', 'Michael', 'Kiran', 'Amanda', 'Joseph', 'Tanzim', 'David', 'Yevgeniy'],
+'rex': ['William', 'Joseph', 'Calvin', 'Ethan', 'Moody', 'Mo', 'Big Mo', 'Peihua', 'Saad', 'Benjamin', 'Justin', 'Alice', 'Hilary', 'Ayham', 'Michael', 'Matthew', 'Jionghao', 'Devin', 'David', 'Jacob', 'Will', 'Hannah', 'Alex'],
+'endymion': ['Grace', 'Nahi', 'Derek', 'Jun Tao', 'Connor', 'Jason', 'Tammy', 'Albert', 'Kazi', 'Derek', 'Brandon', 'Kenneth', 'Lauren', 'Biraj', 'Jeff', 'Jackson', 'Taejoon', 'Kevin', 'Jude', 'Sophie', 'Henry', 'Coby', 'Manfred', 'Leia', 'Ahmed', 'Winston']
 }
 
-def randomName(team):
+#retrieve a random name given the team as a parameter
+def getName(team):
     print (KREWES[team][random.randint(0, len(KREWES[team]) - 1)])
-    #references the team specified by the parameter, then obtains a random number based on the team's length and returns the name at that index
 
-#tests
-#randomName('orpheus')
-#randomName('rex')
-#randomName('endymion')
-
-#different from first function because it does not need team as a parameter, automatically chooses a random team and random name
-#this method was explained to me by Peihua Huang from period 2
-def randomName(dict):
-    teams = list(dict.keys()) #turns dictionary into list
+#retrieve a random team and a random name from that team
+def randomName():
+    teams = list(KREWES.keys()) #turns dictionary into list
     period = random.choice(teams) #chooses a random index from that list
-    print(random.choice(dict[period])) #prints a random index from chosen team
+    print(random.choice(KREWES[period])) #prints a random index from chosen team
 
 #tests
-for i in range(0, 10):
-    randomName(KREWES)
+#for i in range(0, 10):
+#    randomName()
