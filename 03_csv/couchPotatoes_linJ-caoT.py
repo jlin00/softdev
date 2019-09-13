17fcfb3b3f8989220e1487355bb69207db3b1dd6
Jackie Lin <jlin00@stuy.edu>
2019-09-13 09:56:17 -0400

    added rudimentary code to read csv file

---
+++ 03_csv/couchPotatoes_linJ-caoT.py
@@ -0,0 +1,7 @@
+def createDict():
+    dict = {}; #initialize a new dictionary structure
+    file = open("occupations.csv", "r"); #open up csv file for reading purposes
+    content = file.readlines(); #parse through file by line
+    print(content); #test
+
+createDict();
