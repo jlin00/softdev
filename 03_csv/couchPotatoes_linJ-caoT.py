4cc40fcf831b557ff125089bce9325dab4f1b2b9
Jackie Lin <jlin00@stuy.edu>
2019-09-13 22:12:30 -0400

    added code to print each line and spliced some unnecessary lines

--- 03_csv/couchPotatoes_linJ-caoT.py
+++ 03_csv/couchPotatoes_linJ-caoT.py
@@ -1,7 +1,8 @@
-def createDict():
-    dict = {}; #initialize a new dictionary structure
-    file = open("occupations.csv", "r") #open up csv file for reading purposes
-    content = file.readlines() #parse through file by line
-    print(content) #test
-
-createDict();
+#create dictionary based on csv
+dict = {} #initialize a new dictionary structure
+file = open("occupations.csv", "r") #open up csv file for reading purposes
+content = file.readlines() #parse through file by lines
+content = content[1:len(content) - 1] #take out the first and last line
+for line in content:
+    print(line)
+file.close()
