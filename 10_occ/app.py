dd2eda38d475f68604e0ff314935e19e0a4b42e7
Jackie Lin <jlin00@stuy.edu>
2019-09-23 08:31:36 -0400

    added comments to the code and added links for each occupation

--- 10_occ/app.py
+++ 10_occ/app.py
@@ -19,19 +19,22 @@ app = Flask(__name__)
 def occupyflaskst():
     job_dict = dict()
 
-    with open('static/occupations.csv') as file: #see comments in csvreader.py file
-        next(file)
-        reader = csv.reader(file)
+    with open('static/occupations.csv') as file:
+        next(file) #skips first line
+        reader = csv.reader(file) #from imports
         for row in reader:
-            job_dict[row[0]] = float(row[1])
-    del job_dict['Total']
+            job_dict[row[0]] = float(row[1]) #key is title and value is percentage
+    del job_dict['Total'] #remove last line 
 
     random_sel = lambda: random.choices(population=list(job_dict.keys()), weights=list(job_dict.values()), k=1)[0]
+    #shorthand for a function that uses the built-in choices method to randomly select a key
+    #the choices are the dictionary keys and the weights are the values of the key
+    #k = 1 selects on item and returns the name
 
     return render_template('listpage.html',
                            jobs=job_dict.items(),
                            rand_job=random_sel(),
-                           title='Occupations') #passes in dictionary and randomly-selected occupations 
+                           title='Occupations') #passes in dictionary and randomly-selected occupations
 
 
 if __name__ == '__main__':
