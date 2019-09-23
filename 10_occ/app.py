084525a5b405c6ffbfcf715f30ded724bcaba1f2
Jackie Lin <jlin00@stuy.edu>
2019-09-23 19:45:56 -0400

    updated files with changes from class

--- 10_occ/app.py
+++ 10_occ/app.py
@@ -1,11 +1,11 @@
-#Jackie Lin
-#SoftDev1 pd1
-#K10: Jinja Tuning
-#2019-09-24
+# Ivan Galakhov
+# SoftDev1 pd1
+# K10: Jinja Tuning
+# 2019-09-24
 
-#Pair Programming
-#Team Name: France
-#Roster: Ivan Galakhov, Jackie Lin
+# Pair Programming
+# Team Name: France
+# Roster: Ivan Galakhov, Jackie Lin
 
 import csv
 import random
@@ -15,26 +15,27 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 
-@app.route('/occupyflaskst') #returns html page with table of occupations
+@app.route('/occupyflaskst')  # returns html page with table of occupations
 def occupyflaskst():
     job_dict = dict()
 
     with open('static/occupations.csv') as file:
-        next(file) #skips first line
-        reader = csv.reader(file) #from imports
+        next(file)  # skips first line
+        reader = csv.reader(file)  # from imports
         for row in reader:
-            job_dict[row[0]] = float(row[1]) #key is title and value is percentage
-    del job_dict['Total'] #remove last line 
+            job_dict[row[0]] = (float(row[1]), (row[2]))  # key is title and value is percentage
+    del job_dict['Total']  # remove last line
 
-    random_sel = lambda: random.choices(population=list(job_dict.keys()), weights=list(job_dict.values()), k=1)[0]
-    #shorthand for a function that uses the built-in choices method to randomly select a key
-    #the choices are the dictionary keys and the weights are the values of the key
-    #k = 1 selects on item and returns the name
+    # shorthand for a function that uses the built-in choices method to randomly select a key
+    # the choices are the dictionary keys and the weights are the values of the key
+    # k = 1 selects on item and returns the name
+    random_sel = lambda: random.choices(population=[(i, job_dict[i][1]) for i in list(job_dict.keys())],
+                                        weights=[i[0] for i in list(job_dict.values())], k=1)[0]
 
     return render_template('listpage.html',
                            jobs=job_dict.items(),
                            rand_job=random_sel(),
-                           title='Occupations') #passes in dictionary and randomly-selected occupations
+                           title='Occupations')  # passes in dictionary and randomly-selected occupations
 
 
 if __name__ == '__main__':
