012409d508b2d0b4e57f87e365e0d63b8786eee3
Jackie Lin <jlin00@stuy.edu>
2019-10-03 14:37:29 -0400

    added flashing messages

--- 16_flasherit/app.py
+++ 16_flasherit/app.py
@@ -3,7 +3,7 @@
 #K16 -- Oh yes, perhaps I do...
 #2019-10-04
 
-from flask import Flask, render_template, session, url_for, request, redirect
+from flask import Flask, render_template, session, url_for, request, redirect, flash
 import os
 
 app = Flask(__name__)
@@ -17,20 +17,23 @@ password = '123'
 @app.route("/")
 def root():
     if ("user" in session): #if username key exists in session, return welcome page
-        return redirect("/welcome")
+        return redirect(url_for('welcome'))
     else:
-        return redirect("/login") #if user is not logged in, redirect to login form
+        return redirect(url_for('login')) #if user is not logged in, redirect to login form
 
 @app.route("/auth")
 def auth():
     if (username == request.args['username']): #takes in username from form data
         if (password == request.args['password']): #same thing, but with pswd
             session['user'] = username #user has successfully logged in
-            return redirect("/welcome") #go to welcome page
+            flash("You were successfully logged in.")
+            return redirect(url_for('welcome')) #go to welcome page
         else:
-            return render_template("error.html", msg="bad password") #wrong password
+            flash("Bad Password")
+            return render_template("error.html") #wrong password
     else:
-        return render_template("error.html", msg="bad username") #wrong username
+        flash("Bad Username")
+        return render_template("error.html") #wrong username
 
 @app.route("/welcome")
 def welcome():
@@ -48,7 +51,7 @@ def login():
 def logout():
     if ("user" in session):
         session.pop('user') #remove user from session
-    return redirect("/") #return to home page
+    return redirect(url_for('root')) #return to home page
 
 if __name__ == "__main__":
     app.debug = True
