d4a52b951dcd99298652a3a28d5491189a62cac9
Jackie Lin <jlin00@stuy.edu>
2019-10-07 09:51:11 -0400

    downloaded starter files from notes and code

---
+++ 17_csv2db/db_builder.py
@@ -0,0 +1,26 @@
+#Jackie Lin
+#SoftDev pd1
+#K17 -- No Trouble
+#2019-10-08
+
+import sqlite3   #enable control of an sqlite database
+import csv       #facilitate CSV I/O
+
+
+DB_FILE="discobandit.db"
+
+db = sqlite3.connect(DB_FILE) #open if file exists, otherwise create
+c = db.cursor()               #facilitate db ops
+
+#==========================================================
+
+# < < < INSERT YOUR POPULATE-THE-DB CODE HERE > > >
+
+
+command = ""          # test SQL stmt in sqlite3 shell, save as string
+c.execute(command)    # run SQL statement
+
+#==========================================================
+
+db.commit() #save changes
+db.close()  #close database
