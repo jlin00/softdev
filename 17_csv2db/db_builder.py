153759c8eac6e6c7116c082cd7332b9379d91ce1
Jackie Lin <jlin00@stuy.edu>
2019-10-07 10:11:47 -0400

    wrote python code to add csv values to database

--- 17_csv2db/db_builder.py
+++ 17_csv2db/db_builder.py
@@ -15,6 +15,19 @@ c = db.cursor()               #facilitate db ops
 #==========================================================
 
 # < < < INSERT YOUR POPULATE-THE-DB CODE HERE > > >
+with open('courses.csv') as coursefile:
+    reader = csv.DictReader(coursefile) #create new dictreader object
+    c.execute("create table courses (code text, mark integer, id integer);") #create table
+    for row in reader:
+        command = "insert into courses values (\"" + row['code'] + "\", " + row['mark'] + ", " + row['id'] + ");"
+        c.execute(command)
+
+with open('students.csv') as studentfile:
+    reader = csv.DictReader(studentfile) #create new dictreader object
+    c.execute("create table students (code text, mark integer, id integer);") #create table
+    for row in reader:
+        command = "insert into students values (\"" + row['name'] + "\", " + row['age'] + ", " + row['id'] + ");"
+        c.execute(command)
 
 
 command = ""          # test SQL stmt in sqlite3 shell, save as string
