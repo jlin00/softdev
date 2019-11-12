029580bc96271ae62de164cefaa74aee342368b8
Jackie Lin <jlin00@stuy.edu>
2019-11-12 15:03:32 -0500

    fixed ssl certificate problem

--- 24_rest/app.py
+++ 24_rest/app.py
@@ -1,13 +1,28 @@
-from flask import Flask
-import urllib2, json
+#Jackie Lin
+#SoftDev1 pd1
+#K24: A RESTful Journey Skyward
+#2019-11-13
+
+from flask import Flask, render_template
+import urllib.request as urllib2
+#import urllib3 #second method using urllib3
+import json
 app = Flask(__name__)
 
 @app.route("/")
 def root():
-    u = urllib2.urlopen("https://api.nasa.gov/planetary/earth/imagery/?lon=100.75&lat=1.5&date=2014-02-01&cloud_score=True&api_key=tcePwmse95ZYuZ2kBWPenl6klaR3BCoKWm86eehP")
+    u = urllib2.urlopen("https://api.nasa.gov/planetary/earth/imagery/?lon=-74.011146&lat=40.71139&cloud_score=0&api_key=tcePwmse95ZYuZ2kBWPenl6klaR3BCoKWm86eehP")
     response = u.read()
     data = json.loads(response)
-    return render-template("index.html", pic=data['url'])
+
+    #using urllib3
+
+    #url = "https://api.nasa.gov/planetary/earth/imagery/?lon=-74.011146&lat=40.71139&cloud_score=0&api_key=tcePwmse95ZYuZ2kBWPenl6klaR3BCoKWm86eehP"
+    #http = urllib3.PoolManager()
+    #response = http.request('GET', url)
+    #data = json.loads(response.data)
+
+    return render_template("index.html", pic=data['url'])
 
 if __name__ == "__main__":
     app.debug = True
