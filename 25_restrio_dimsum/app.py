5bd74a1695609a67cd8ae77ddfdc97a24875e28f
Jackie Lin <jlin00@stuy.edu>
2019-11-13 23:41:03 -0500

    added in Tiffany's contributions

--- 25_restrio_dimsum/app.py
+++ 25_restrio_dimsum/app.py
@@ -1,4 +1,4 @@
-#Jackie Lin, Tiffany Cao 
+#Jackie Lin, Tiffany Cao
 #SoftDev1 pd1
 #K25: Getting More REST
 #2019-11-13
@@ -36,7 +36,32 @@ def countries():
     data = json.loads(response)
     return render_template("countries.html", name=data['name'],cor=data['latlng'],region=data['region'],capital=data['capital'],pop=data['population'],flag=data['flag'])
 
+@app.route('/addressautocomplete')
+def address():
+    u = urllib2.urlopen(
+    "https://us-autocomplete.api.smartystreets.com/suggest?auth-id=2676fc89-0bed-fde9-0ff6-b70829031c6c&auth-token=022h0rkvcmLqMZ7EXEii&prefix=4770%20Lincoln%20Ave%20O"
+    )
+    response = u.read()
+    data = json.loads(response)
+    data = data['suggestions']
+    # stuff = stuff['href']
+    return render_template("address.html", stuff = data, length = len(data))
 
+@app.route("/airquality")
+def air():
+    url = "http://api.airvisual.com/v2/city?city=Los%20Angeles&state=California&country=USA&key=e6ce6055-e9ac-4bf4-add0-3cff6f47a5ec"
+    u = urllib2.urlopen(url)
+    response = u.read()
+    data = json.loads(response)
+    return render_template("airquality.html", city = data['data']['city'], state = data['data']['state'], country = data['data']['country'], poll = data['data']['current'], length = len(data['data']['current']))
+
+@app.route("/trivia")
+def trivia():
+    url = "https://opentdb.com/api.php?amount=2"
+    u = urllib2.urlopen(url)
+    response = u.read()
+    data = json.loads(response)
+    return render_template("trivia.html", category = data['results'][0], category2 = data['results'][1])
 
 if __name__ == "__main__":
     app.debug = True
