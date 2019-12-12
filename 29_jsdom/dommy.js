2681b7ea9ff12e7320c32c655b38472128c7f8a1
Jackie Lin <jlin00@stuy.edu>
2019-12-12 09:33:45 -0500

    pushed changeHeading

---
+++ 29_jsdom/dommy.js
@@ -0,0 +1,54 @@
+var changeHeading = function(e) {
+  var h = document.getElementById("h");
+  console.log(e.srcElement.innerHTML);
+  h.innerHTML = e.srcElement.innerHTML;
+};
+
+var removeItem = function(e) {
+  ///
+};
+
+var lis = document.getElementsByTagName("li");
+
+for (var i=0; i < lis.length; i++){
+  lis[i].addEventListener('mouseover', changeHeading);
+  console.log(i);
+  lis[i].addEventListener('mouseout', () => {
+    var h = document.getElementById("h");
+    h.innerHTML = "Hello World!";
+  });
+  lis[i].addEventListener('click', removeItem);
+}
+
+var addItem = function(e) {
+  //var list = ???;
+  var item = document.createElement("li");
+  //??? = "WORD";
+  //list.???(item);
+};
+
+var button = document.getElementById("b");
+button.addEventListener('click', addItem);
+
+var fib = function(n) {
+  if (n < 2){
+    return 1;
+  }
+  else {
+    return fib(n-1) + fib(n-2);
+  }
+};
+
+var addFib = function(e) {
+  console.log(e);
+  ///???
+};
+
+var addFib2 = function(e) {
+  console.log(e);
+  ///???
+  //DYNAMIC PROGRAMMING
+};
+
+var fb = document.getElementById("fb");
+fb.addEventListener("click", addFib);
