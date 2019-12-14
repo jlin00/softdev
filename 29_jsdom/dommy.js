592cafc605b4e325bcce8c56476fb09fbcb274d1
Jackie Lin <jlin00@stuy.edu>
2019-12-13 22:32:09 -0500

    changed to this keyword

--- 29_jsdom/dommy.js
+++ 29_jsdom/dommy.js
@@ -5,11 +5,11 @@
 
 var changeHeading = function(e) {
   var h = document.getElementById("h");
-  h.innerHTML = e.srcElement.innerHTML;
+  h.innerHTML = this.name;
 };
 
 var removeItem = function(e) {
-  e.srcElement.remove();
+  this.remove();
 };
 
 //helper function for adding event listeners to a list item
@@ -23,35 +23,38 @@ var addEvents = function(element) {
 }
 
 var lis = document.getElementsByTagName("li");
+//console.log(lis);
 
 for (var i=0; i < lis.length; i++){
+  lis[i].name=lis[i].innerHTML;
   addEvents(lis[i]);
 }
 
 var addItem = function(e) {
   var list = document.getElementById("thelist");
   var item = document.createElement("li");
+  //console.log(this);
+  item.name = "WORD";
   item.innerHTML = "WORD";
   addEvents(item);
   list.appendChild(item);
+  lis = document.getElementsByTagName("li");
+  //console.log(lis);
 };
 
 var button = document.getElementById("b");
 button.addEventListener('click', addItem);
 
 var fib = function(n) {
-  if (n < 2) {
-    return 1;
-  }
-  else {
-    return fib(n - 1) + fib(n - 2);
-  }
-};
+  if (n < 2) return 1;
+  return fib(n - 1) + fib(n - 2);
+}
 
 var addFib = function(e) {
-  //console.log(e);
+  console.log(e);
   var fiblist = document.getElementById("fiblist");
   var n = fiblist.getElementsByTagName("li").length;
+  //console.log(fib(2));
   var item = document.createElement("li");
   item.innerHTML = fib(n);
   fiblist.appendChild(item);
@@ -65,6 +68,7 @@ var fib2 = function(n){
     return fibarray[n];
   }
   fibarray.push(fibarray[n-1] + fibarray[n-2]);
+  //console.log(fibarray);
   return fibarray[n];
 };
 
