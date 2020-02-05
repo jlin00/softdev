b0bee201448c85d5ce36d44259da69a44f395f77
Amanda Zheng <azheng00@stuy.edu>
2020-02-05 15:22:47 -0500

    colors

--- 03_canvas/clickdraw.js
+++ 03_canvas/clickdraw.js
@@ -5,7 +5,10 @@
 
 var c = document.getElementById("slate");
 var ctx = c.getContext("2d");
-ctx.fillStyle = "#0000ff";
+var colors=["black","green","yellow","purple","red","blue","brown","orange","pink","grey"]
+var colorIndex=0
+ctx.fillStyle = colors[colorIndex]
+ctx.strokeStyle = colors[colorIndex]
 var mode = 0; //0 for dot, 1 for box
 
 var clear = function(){
@@ -25,11 +28,19 @@ var toggle = function(){
   //console.log(mode);
 }
 
+var colorize = function(){
+  //console.log("hi")
+  colorIndex=(colorIndex+1)%10;
+  ctx.fillStyle=colors[colorIndex];
+  ctx.strokeStyle = colors[colorIndex];
+  document.getElementById("CurrentColor").innerHTML = colors[colorIndex];
+}
+
 c.addEventListener('mousedown', e => {
   var x = e.pageX - c.offsetLeft;
   var y = e.pageY - c.offsetTop;
   if (mode == 1){
-    ctx.fillRect(x, y, 20, 20);
+    ctx.fillRect(x-10, y-10, 20, 20);
   }
   else {
     ctx.beginPath();
@@ -44,3 +55,6 @@ clearbtn.addEventListener('click', clear);
 
 var togglebtn = document.getElementById("toggle");
 togglebtn.addEventListener('click', toggle);
+
+var colorizebtn = document.getElementById("color");
+colorizebtn.addEventListener('click', colorize);
