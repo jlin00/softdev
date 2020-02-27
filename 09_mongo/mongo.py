72c5a4f5331e69be5efc47febb0d08ee54c5c1a2
Jackie Lin <jlin00@stuy.edu>
2020-02-27 10:05:03 -0500

    syntax

--- 09_mongo/mongo.py
+++ 09_mongo/mongo.py
@@ -72,7 +72,7 @@ while (not zip.isNumeric() or int(zip) / 10000 < 1):
     print("Please enter a valid zipcode!")
     zip = input("Please enter a zipcode: \n")
 number = input("Please enter the maximum number of results you want to return: \n")
-while (not number.isNumeric():
+while (not number.isNumeric()):
     print("Please enter a valid  number!")
     number = input("Please enter the maximum number of results you want to return: \n")
 for rest in find_num_zip(zip, int(number)):
