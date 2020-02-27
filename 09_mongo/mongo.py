29c71a154d63c2a85d26942bae1242d985af3b8c
Jackie Lin <jlin00@stuy.edu>
2020-02-27 08:41:39 -0500

    syntax err

--- 09_mongo/mongo.py
+++ 09_mongo/mongo.py
@@ -68,15 +68,13 @@ for rest in find_zip_score("10019", 4):
 
 print("------------FINDING N RESTAURANTS IN GIVEN ZIPCODE------------")
 zip = raw_input("Please enter a zipcode: \n")
-while (zip == ""){
+while (zip == ""):
     print("Please enter a non-empty string!")
     zip = raw_input("Please enter a zipcode: \n")
-}
 number = raw_input("Please enter the maximum number of results you want to return: \n")
-while (number == ""){
+while (number == ""):
     print("Please enter a non-empty string!")
     number = raw_input("Please enter a zipcode: \n")
-}
 for rest in find_num_zip(zip, number):
     for key, value in rest.items():
         if key == "name":
