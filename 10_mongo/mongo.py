2ff31ac6cd2b86b88077ed1591c2542aadaa5e57
Jackie Lin <jlin00@stuy.edu>
2020-03-01 00:33:11 -0500

    started writing script

---
+++ 10_mongo/mongo.py
@@ -0,0 +1,19 @@
+#Kenneth Chin, Jackie Lin
+#SoftDev1 pd1
+#K10 -- Import/Export Bank
+#2020-03-04
+
+import json
+from pymongo import MongoClient
+
+client = MongoClient()
+db = client.test
+events = db.events
+if (events.count() == 0):
+    with open('dataset.json') as file:
+    data = json.load(file)
+    for event in data['results']:
+        events.insert_one(event)
+
+for item in events.find({}).limit(20):
+    print(item)
